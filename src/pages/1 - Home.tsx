6ead723ae491db2a4489c4cc759b71855d2ec27b
Jeremiah Weaver <jbw716@users.noreply.github.com>
2025-02-08 15:00:16 -0600

    Styling with emergency contact info on home screen.

--- src/pages/1 - Home.tsx
+++ src/pages/1 - Home.tsx
@@ -8,7 +8,7 @@ import { EffectCards } from 'swiper/modules';
 import { call, chatbubble, globe } from 'ionicons/icons';
 
 const verticalCenterStyle: CSSProperties = {
-  height: '100%',
+  // height: '100%',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
@@ -24,50 +24,69 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent scrollX={false} scrollY={false} style={{ overflow: 'hidden' }}>
-        <IonList>
-          <IonItem lines="none">
-            <IonLabel slot="start">National Suicide and Crisis Lifeline</IonLabel>
-            <span slot="end">
-              <IonButton fill="clear" size="large">
-                <IonIcon slot="icon-only" icon={chatbubble}></IonIcon>
-              </IonButton>
-              <IonButton fill="clear" size="large">
-                <IonIcon slot="icon-only" icon={call}></IonIcon>
-              </IonButton>
-              <IonButton fill="clear" size="large">
-                <IonIcon slot="icon-only" icon={globe}></IonIcon>
-              </IonButton>
-            </span>
-          </IonItem>
-        </IonList>
-        <div style={verticalCenterStyle}>
-          <Swiper
-            style={{ marginTop: '-25%', height: '50%' }}
-            direction='vertical'
-            slidesPerView={1}
-            spaceBetween={30}
-            modules={[EffectCards]}
-            effect={'cards'}
-          >
-            <SwiperSlide style={verticalCenterStyle}>
-              <img src='assets/img/license.png' />
-            </SwiperSlide>
-            <SwiperSlide style={verticalCenterStyle}>
-              <img src='assets/img/CarRegistration.jpg' />
-            </SwiperSlide>
-            <SwiperSlide style={verticalCenterStyle}>
-              <img src='assets/img/ConcealedHandgun.jpg' />
-            </SwiperSlide>
-            <SwiperSlide style={verticalCenterStyle}>
-              <img src='assets/img/LDFW.jpg' />
-            </SwiperSlide>
-            <SwiperSlide style={verticalCenterStyle}>
-              <img src='assets/img/Medicaid.jpg' />
-            </SwiperSlide>
-            <SwiperSlide style={verticalCenterStyle}>
-              <img src='assets/img/SMARTHealth.jpg' />
-            </SwiperSlide>
-          </Swiper>
+        <div style={{ ...verticalCenterStyle, height: '100%' }}>
+          <IonList style={{ flex: '0 0 auto', width: '100%' }}>
+            <IonItem>
+              <IonLabel color="light">National Suicide<br />and Crisis Lifeline</IonLabel>
+              <span slot="end">
+                <IonButton fill="clear" size="large">
+                  <IonIcon slot="icon-only" icon={chatbubble} color="light"></IonIcon>
+                </IonButton>
+                <IonButton fill="clear" size="large">
+                  <IonIcon slot="icon-only" icon={call} color="light"></IonIcon>
+                </IonButton>
+                <IonButton fill="clear" size="large">
+                  <IonIcon slot="icon-only" icon={globe} color="light"></IonIcon>
+                </IonButton>
+              </span>
+            </IonItem>
+            <IonItem>
+              <IonLabel color="light">Report Human Trafficking</IonLabel>
+              <span slot="end">
+                <IonButton fill="clear" size="large">
+                  <IonIcon slot="icon-only" icon={call} color="light"></IonIcon>
+                </IonButton>
+                <IonButton fill="clear" size="large">
+                  <IonIcon slot="icon-only" icon={globe} color="light"></IonIcon>
+                </IonButton>
+              </span>
+            </IonItem>
+            <IonItem>
+              <IonLabel color="light">Louisiana Child<br />Abuse/Neglect Hotline</IonLabel>
+              <span slot="end">
+                <IonButton fill="clear" size="large">
+                  <IonIcon slot="icon-only" icon={call} color="light"></IonIcon>
+                </IonButton>
+              </span>
+            </IonItem>
+          </IonList>
+          <div style={{ ...verticalCenterStyle, flex: '1 1 100%', overflow: 'hidden', height: '100%' }}>
+            <Swiper
+              modules={[EffectCards]}
+              effect={'cards'}
+              direction='vertical'
+              style={{ maxHeight: '300px' }}
+            >
+              <SwiperSlide style={verticalCenterStyle}>
+                <img src='assets/img/license.png' />
+              </SwiperSlide>
+              <SwiperSlide style={verticalCenterStyle}>
+                <img src='assets/img/CarRegistration.jpg' />
+              </SwiperSlide>
+              <SwiperSlide style={verticalCenterStyle}>
+                <img src='assets/img/ConcealedHandgun.jpg' />
+              </SwiperSlide>
+              <SwiperSlide style={verticalCenterStyle}>
+                <img src='assets/img/LDFW.jpg' />
+              </SwiperSlide>
+              <SwiperSlide style={verticalCenterStyle}>
+                <img src='assets/img/Medicaid.jpg' />
+              </SwiperSlide>
+              <SwiperSlide style={verticalCenterStyle}>
+                <img src='assets/img/SMARTHealth.jpg' />
+              </SwiperSlide>
+            </Swiper>
+          </div>
         </div>
       </IonContent>
     </IonPage>

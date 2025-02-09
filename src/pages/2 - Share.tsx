de678213035c4ffd3ffa2ed42b3a1d3afd9467f2
Jeremiah Weaver <jbw716@users.noreply.github.com>
2025-02-08 21:47:33 -0600

    Added verify you qr code.

--- src/pages/2 - Share.tsx
+++ src/pages/2 - Share.tsx
@@ -1,21 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonContent, IonHeader, IonIcon, IonImg, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
 import './2 - Share.css';
-import { useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
+import { informationCircle } from 'ionicons/icons';
+import { QRCode } from 'react-qrcode-logo';
 
 const Share: React.FC = () => {
 
   const [segment, setSegment] = useState('verify');
+  const [qrValue, setQrValue] = useState(crypto.randomUUID());
 
-  function getSegment() {
-    switch (segment) {
-      case 'verify':
-        return 'Verify You';
-      case 'remote':
-        return 'Remote Verify';
-      default:
-        return 'Uh oh...s';
-    }
+  useEffect(() => {
+    const timerId = setInterval(() => setQrValue(crypto.randomUUID()), 1000);
+    return () => clearInterval(timerId);
+  });
+
+  const contentContainerStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+    height: '100%'
   }
 
   return (
@@ -25,8 +30,8 @@ const Share: React.FC = () => {
           <IonTitle className='ion-text-center'>Share</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonToolbar color="primary">
+      <IonContent style={contentContainerStyle}>
+        <IonToolbar color="primary" style={{ flex: '0 0 auto', width: '100%' }}>
           <IonSegment value={segment} onIonChange={e => setSegment(e.detail.value as string)}>
             <IonSegmentButton color="light" value="verify">
               Verify You
@@ -36,7 +41,27 @@ const Share: React.FC = () => {
             </IonSegmentButton>
           </IonSegment>
         </IonToolbar>
-        <ExploreContainer name={`${getSegment()} page`} />
+        <div>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle style={{ textAlign: 'center' }}>
+                To use Verify You, present the below QR code for scanning.
+                Select whether you would like to share you name, and, and/or driver's license number.
+                <br />
+                <br />
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                  <IonIcon icon={informationCircle}></IonIcon>&nbsp; What is Verify You?
+                </div>
+              </IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <div style={{ position: 'relative', height: '200px' }}>
+                <IonImg src='assets/img/face.png' style={{ left: '50%', transform: 'translateX(-50%)', width: '200px', height: '200px', 'object-fit': 'cover', position: 'absolute' }}></IonImg>
+                <QRCode value={qrValue} size={200} ecLevel='H' qrStyle='dots' bgColor='transparent' style={{ left: '50%', top: '-10px', transform: 'translateX(-50%)', position: 'absolute' }}></QRCode>
+              </div>
+            </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );

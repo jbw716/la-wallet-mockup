dd265ba8a27015541d758569b5a318bbf030a327
Jeremiah Weaver <jbw716@users.noreply.github.com>
2025-02-08 22:34:17 -0600

    Share screen styling.

--- src/pages/2 - Share.tsx
+++ src/pages/2 - Share.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonContent, IonHeader, IonIcon, IonImg, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCheckbox, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonList, IonPage, IonSegment, IonSegmentButton, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import './2 - Share.css';
 import { CSSProperties, useEffect, useState } from 'react';
 import { informationCircle } from 'ionicons/icons';
@@ -41,9 +41,9 @@ const Share: React.FC = () => {
             </IonSegmentButton>
           </IonSegment>
         </IonToolbar>
-        <div>
-          <IonCard>
-            <IonCardHeader>
+        <IonList>
+          <IonCard style={{ marginBottom: '10px' }}>
+            <IonCardHeader style={{ border: 'none' }}>
               <IonCardSubtitle style={{ textAlign: 'center' }}>
                 To use Verify You, present the below QR code for scanning.
                 Select whether you would like to share you name, and, and/or driver's license number.
@@ -55,15 +55,90 @@ const Share: React.FC = () => {
               </IonCardSubtitle>
             </IonCardHeader>
             <IonCardContent>
-              <div style={{ position: 'relative', height: '200px' }}>
+              <div style={{ position: 'relative', height: '220px' }}>
                 <IonImg src='assets/img/face.png' style={{ left: '50%', transform: 'translateX(-50%)', width: '200px', height: '200px', 'object-fit': 'cover', position: 'absolute' }}></IonImg>
                 <QRCode value={qrValue} size={200} ecLevel='H' qrStyle='dots' bgColor='transparent' style={{ left: '50%', top: '-10px', transform: 'translateX(-50%)', position: 'absolute' }}></QRCode>
               </div>
             </IonCardContent>
           </IonCard>
-        </div>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <IonCard style={{ flex: '1 1 50%' }}>
+              <IonCardHeader>
+                <IonCardSubtitle>
+                  My Portrait
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonItem>
+                  <IonLabel>
+                    <strong>Yes</strong>
+                  </IonLabel>
+                </IonItem>
+              </IonCardContent>
+            </IonCard>
+            <IonCard style={{ flex: '1 1 50%' }}>
+              <IonCardHeader>
+                <IonCardSubtitle>
+                  License Status
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonItem>
+                  <IonLabel>
+                    <strong>Valid</strong>
+                  </IonLabel>
+                </IonItem>
+              </IonCardContent>
+            </IonCard>
+          </div>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>
+                Age
+              </IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonItem>
+                <IonLabel>
+                  <strong>Over 25</strong>
+                </IonLabel>
+                <IonCheckbox slot="end" checked={true}></IonCheckbox>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>
+                Name
+              </IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonItem>
+                <IonLabel>
+                  <strong>John Envoc Doe</strong>
+                </IonLabel>
+                <IonCheckbox slot="end" checked={true}></IonCheckbox>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>
+                License Number
+              </IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonItem>
+                <IonLabel>
+                  <strong>999999996</strong>
+                </IonLabel>
+                <IonCheckbox slot="end" checked={true}></IonCheckbox>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+        </IonList>
       </IonContent>
-    </IonPage>
+    </IonPage >
   );
 };
 

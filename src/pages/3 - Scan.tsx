e06cd00ede47b5054c92b7850091f855e9190823
Jeremiah Weaver <jbw716@users.noreply.github.com>
2025-02-15 11:29:24 -0600

    Basic QR scanner functionality.

--- src/pages/3 - Scan.tsx
+++ src/pages/3 - Scan.tsx
@@ -1,8 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonButtons, IonContent, IonHeader, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './3 - Scan.css';
+import { Scanner } from '@yudiel/react-qr-scanner';
+import { useEffect, useRef, useState } from 'react';
 
 const Scan: React.FC = () => {
+  const modal = useRef<HTMLIonModalElement>(null);
+  const page = useRef(null);
+
+  const [presentingElement, setPresentingElement] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState('');
   return (
     <IonPage>
       <IonHeader>
@@ -11,7 +23,29 @@ const Scan: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <ExploreContainer name="Scan page" />
+        <Scanner formats={['qr_code']}
+          components={{ zoom: true }}
+          styles={{ container: { width: '100%', height: 'auto' }, video: { width: '100%', height: 'auto' } }}
+          onScan={(data) => {
+            console.log(data);
+            setData(data[0].rawValue)
+            setIsOpen(true);
+          }} />
+        <IonModal isOpen={isOpen} presentingElement={presentingElement!} ref={modal}>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Modal</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <p>
+              {data}
+            </p>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );

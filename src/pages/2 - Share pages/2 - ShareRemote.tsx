3001bb79e4802c04b6febf8157628d2dd1bf23d4
Jeremiah Weaver <jbw716@users.noreply.github.com>
2025-02-20 08:06:11 -0600

    Added basic remote verify screen.

--- src/pages/2 - Share pages/2 - ShareRemote.tsx
+++ src/pages/2 - Share pages/2 - ShareRemote.tsx
@@ -1,18 +1,37 @@
-// import './2 - Share.css';
-import { useEffect, useState } from 'react';
-import ExploreContainer from '../../components/ExploreContainer';
+import './2 - ShareRemote.css';
+import { IonButton, IonIcon, IonInput, IonItem, IonList, IonText } from '@ionic/react';
+import { informationCircle } from 'ionicons/icons';
+import { useState } from 'react';
 
 const ShareRemote: React.FC = () => {
-
-  const [qrValue, setQrValue] = useState(crypto.randomUUID());
-
-  useEffect(() => {
-    const timerId = setInterval(() => setQrValue(crypto.randomUUID()), 1000);
-    return () => clearInterval(timerId);
-  });
+  const [code, setCode] = useState<string | null | undefined>();
 
   return (
-    <ExploreContainer name="Share Remote" />
+    <IonList lines="none" style={{ position: 'absolute', top: '20%' }}>
+      <IonItem lines="none">
+        <IonIcon icon={informationCircle} slot="start" color="light" style={{ margin: '0 1em 0 0' }}></IonIcon>
+        <IonText color="light">
+          Enter the Verify You code the requester has provided to verify your identity remotely.
+        </IonText>
+      </IonItem>
+      <IonItem>
+        {/* <IonCard style={{ width: '100%' }}>
+          <IonCardHeader>
+            <IonCardTitle style={{ textAlign: 'center' }}>
+              Remote Verify You Code
+            </IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent style={{ padding: '1em' }}>
+            <IonInput placeholder="Enter Verify You Code" clearOnEdit={true} fill="solid" style={{ fontSize: 'x-large', textAlign: 'center' }} maxlength={4} minlength={4} />
+          </IonCardContent>
+        </IonCard> */}
+
+        <IonInput placeholder="Enter Verify You Code" clearOnEdit={true} color="light" fill="outline" style={{ fontSize: 'x-large', textAlign: 'center', color: 'var(--ion-color-light)' }} maxlength={4} minlength={4} onIonInput={input => setCode(input.detail.value)} />
+      </IonItem>
+      <IonButton color="light" style={{ width: '100%', padding: '2em', margin: 0 }} disabled={!code || code.length < 4}>
+        Submit
+      </IonButton>
+    </IonList>
   );
 };
 
